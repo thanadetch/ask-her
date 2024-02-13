948f1365e99d36993a390bf44a33aa91493710cd
Thanadetch Lertwuthikarn <tlertwuthikarn@palo-it.com>
2024-02-13 23:35:30 +0700

    update project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
             )}
             {isOk && <div className={"flex flex-col gap-4 px-4 text-pink-400 text-3xl mt-[-50px]"}>
                 <div>
-                    My love, You are my everything.
+                    My love, You are everything for me.
                 </div>
                 <div>
                     I wish to wake up and see your beautiful face every morning.
